5fbf0ef21d900c75568e1aac6b34ac8548d39765
YoussefNoaman96 <youssefnoaman96@gmail.com>
2026-08-30 17:48:20 +0300

    style(home): update home page layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-4 px-6 py-16 lg:px-8">
         <h1 className="font-display text-5xl font-bold tracking-tight text-(--color-text-primary) sm:text-6xl">
-          E-commerce Home page
+          E-commerce Home page, hello guys
         </h1>
     </div>
   )}

7281e7d5221f3ef770390e61351452a33f7535a4
Deema Alfaqawi <deemasameh8@gmail.com>
2026-08-31 11:35:53 +0300

    W1-04: connect protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home.jsx";
 import { Routes, Route } from "react-router";
 import Dashboard from "./pages/Dashboard.jsx";
 import Login from "./pages/Login.jsx";
+import ProtectedRoute from "./ProtectedRoute.jsx";
 
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/desgin-system" element={<DesignSystem />} />
       <Route path="/login" element={<Login />} /> 
-      <Route path="/dashboard/*" element={<Dashboard />} />
+      <Route path="/dashboard/*" element={<ProtectedRoute isAuthenticated={true} user={{ role: "admin" }}/>} >
+       <Route index element={<Dashboard />} />
+      </Route>
     </Routes>
   );
 }
--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import {Navigate , Outlet} from 'react-router-dom';
+import { Navigate, Outlet } from "react-router";
 const ProtectedRoute = ({ isAuthenticated , user}) => {
     if (!isAuthenticated){
         return <Navigate replace to="/login"/>;

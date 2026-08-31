1d1627a5dfadd47d50cfe7146af0603b611118d5
mahmoudMokhtar-94 <mahmoudformal94@gmail.com>
2026-08-31 20:06:31 +0300

    feat:add admin layout and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import DesignSystem from "./pages/DesignSystem";
 import Home from "./pages/Home.jsx";
+import AdminLayout from "./components/layout/AdminLayout";
 import { Routes, Route } from "react-router";
+import Dashboard from "./pages/Dashboard";
+import Products from "./pages/Products";
+import Orders from "./pages/Orders";
+import Users from "./pages/Users";
+import Carts from "./pages/Carts";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/desgin-system" element={<DesignSystem />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/design-system" element={<DesignSystem />} />
+        {/* Start of Nested Routes  */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="products" element={<Products />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="users" element={<Users />} />
+          <Route path="carts" element={<Carts />} />
+        </Route>
+        {/* End of Nested Routes  */}
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/Orders.jsx
@@ -0,0 +1,7 @@
+export default function Orders() {
+  return (
+    <div>
+      <h1 style={{ color: "blue" }}>Orders Page</h1>
+    </div>
+  );
+}

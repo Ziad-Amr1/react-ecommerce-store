683229abdfa50bd479725ce44c4a38fe350aafeb
Deema Alfaqawi <deemasameh8@gmail.com>
2026-08-31 10:27:39 +0300

    W1-04: add admin route structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import DesignSystem from "./pages/DesignSystem";
 import Home from "./pages/Home.jsx";
 import { Routes, Route } from "react-router";
+import Dashboard from "./pages/Dashboard.jsx";
+import Login from "./pages/Login.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/desgin-system" element={<DesignSystem />} />
+      <Route path="/login" element={<Login />} /> 
+      <Route path="/dashboard/*" element={<Dashboard />} />
     </Routes>
   );
 }
 
-export default App;
+export default App; 

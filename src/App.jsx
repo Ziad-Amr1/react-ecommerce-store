234966e3cce29a96cd66506d3a4f39699c5d124d
Ziad Amr <Zyadamr177@gmail.com>
2026-09-01 15:50:01 +0300

    Revert PR #8: restore W1-02 auth contract

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,30 @@
 import DesignSystem from "./pages/DesignSystem";
 import Home from "./pages/Home.jsx";
 import AdminLayout from "./components/layout/AdminLayout";
-import { Routes, Route, Navigate } from "react-router-dom";
-
+import { Routes, Route } from "react-router";
 import Dashboard from "./pages/Dashboard";
 import Products from "./pages/Products";
 import Orders from "./pages/Orders";
 import Users from "./pages/Users";
 import Carts from "./pages/Carts";
 
-import Login from "./pages/Login.jsx";
-
 function App() {
   return (
-    <Routes>
-      {/* Home */}
-      <Route path="/" element={<Home />} />
-
-      {/* Design System */}
-      <Route
-        path="/design-system"
-        element={<DesignSystem />}
-      />
-
-      {/* Login */}
-      <Route
-        path="/login"
-        element={<Login />}
-      />
-
-      {/* Admin */}
-      <Route
-        path="/admin"
-        element={<AdminLayout />}
-      >
-        <Route index element={<Dashboard />} />
-
-        <Route
-          path="products"
-          element={<Products />}
-        />
-
-        <Route
-          path="orders"
-          element={<Orders />}
-        />
-
-        <Route
-          path="users"
-          element={<Users />}
-        />
-
-        <Route
-          path="carts"
-          element={<Carts />}
-        />
-      </Route>
-
-      {/* أي URL مش موجود */}
-      <Route
-        path="*"
-        element={<Navigate to="/" replace />}
-      />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/design-system" element={<DesignSystem />} />
+        {/* Start of Nested Routes  */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="products" element={<Products />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="users" element={<Users />} />
+          <Route path="carts" element={<Carts />} />
+        </Route>
+        {/* End of Nested Routes  */}
+      </Routes>
+    </>
   );
 }
 

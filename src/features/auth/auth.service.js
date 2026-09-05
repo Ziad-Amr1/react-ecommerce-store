186efe604d8ba79b6893eb279ef6c708d0f70b4f
Ziad Amr <Zyadamr177@gmail.com>
2026-09-05 16:02:25 +0300

    refactor(auth): route auth HTTP through auth service

--- src/features/auth/auth.service.js
+++ src/features/auth/auth.service.js
@@ -1,5 +1,26 @@
 import api from "@/api/axios";
 
+export const loginUser = async (email, password) => {
+  const response = await api.post("/auth/login", {
+    email,
+    password,
+  });
+
+  return response.data;
+};
+
+export const logoutUser = async () => {
+  const response = await api.post("/auth/logout");
+
+  return response.data;
+};
+
+export const getCurrentUser = async () => {
+  const response = await api.get("/auth/me");
+
+  return response.data;
+};
+
 export const sendForgotPasswordOTP = async (email) => {
   const response = await api.post("/auth/forgot-password/send-otp", {
     email,
